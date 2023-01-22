6fda654d30eedf863cbb50cca76b1fb1ab160d74
Nikolai Vazquez <hello@nikolaivazquez.com>
2023-01-21 23:49:08 -0500

    Fix inconsistent `concat!` formatting
    For empty args, the `Debug` impl would emit `""`.
    
    For single arg, the `Display` impl could be padded. Padding as an
    adapter is planned for later.

--- src/concat.rs
+++ src/concat.rs
@@ -321,11 +321,6 @@ impl_tuple!(T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11);
 ///
 /// This is like [`concat_tuple()`] but with no length limit.
 ///
-/// As an optimization, this macro may return its input as-is or [`&str`](str)
-/// (if empty). Although this will not change in the future, it is good practice
-/// to treat the output type as an opaque <code>impl [Display]</code> rather
-/// than rely on the concrete type.
-///
 /// # Examples
 ///
 /// ```
@@ -335,8 +330,8 @@ impl_tuple!(T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11);
 /// ```
 #[macro_export]
 macro_rules! concat {
-    () => { "" };
-    ($t0:expr $(,)?) => { $t0 };
+    () => { $crate::concat_tuple(()) };
+    ($t0:expr $(,)?) => { $crate::concat_tuple(($t0,)) };
     ($t0:expr, $t1:expr, $t2:expr, $t3:expr, $t4:expr, $t5:expr, $t6:expr, $t7:expr, $t8:expr, $t9:expr, $t10:expr $(, $rest:expr)+ $(,)?) => {
         $crate::concat_tuple(($t0, $t1, $t2, $t3, $t4, $t5, $t6, $t7, $t8, $t9, $t10, $crate::concat!($($rest),+)))
     };
